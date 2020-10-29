19531d321caf7ffee5116fde9b5341d1133b6d53
grace <grace.c.youngblood@gmail.com>
2020-10-29 01:10:55 -0400

    parse us code as a code hierarchy.

---
+++ lib/parse/usc.rb
@@ -0,0 +1,70 @@
+require "nokogiri"
+
+module Parse
+    LEVELS = %w[
+        preliminary
+        title
+        subtitle
+        chapter
+        subchapter
+        part
+        subpart
+        division
+        subdivision
+        article
+        subarticle
+        section
+        subsection
+        paragraph
+        subparagraph
+        clause
+        subclause
+        item
+        subitem
+        subsubitem
+    ]
+
+    HEADINGS = ["document", "uscDoc", "main"]
+
+    class USC
+        def initialize(path)
+            @sources = Dir.glob(File.join(path, "/*.xml")).sort
+        end
+
+        attr_accessor :sources
+
+        def parse
+            debug = true
+            sources.map do |s|
+                puts s
+                xml = Nokogiri::XML(File.read(s))
+                response = []
+                xml.traverse do |x|
+                    if ([x.name] + x.ancestors.map(&:name)).all? {|n| (HEADINGS + LEVELS).include? n }
+                        body = x.search("content")[0].text rescue x.text
+                        hash = Digest::SHA2.hexdigest(body)
+                        Measure.create!(
+                            name: x.name,
+                            key: x.attr("identifier"),
+                            hash: hash,
+                            body: body,
+                            label: (x.search("num")[0].attr("value") rescue nil),
+                            heading: (x.search("heading")[0].text rescue nil),
+                        )
+                        unless HEADINGS.include? x.name
+                            x.replace("{measure '#{hash}'}")
+                        end
+                    end
+                end
+                response
+            end
+        end
+
+        def load
+        end
+    end
+end
+
+def go
+    Parse::USC.new("./source").parse
+end
